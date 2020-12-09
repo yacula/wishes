93f5d75124f3fd110431b3477bf4e151cc43827d
Piotrowski, Jacek (CTI-TMP) <Jacek.Piotrowski@bshg.com>
2020-12-09 13:37:43 +0100

    first commit

---
+++ frontend/src/components/Content/Content.js
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import WishesStepper from "../WishesStepper/WishesStepper";
+
+const Content = () => {
+  return <main id="mainContent">
+    <div className="container">
+      <div className="row justify-content-center mt-5 p-0">
+        <WishesStepper/>
+      </div>
+    </div>
+  </main>;
+}
+export default Content;
